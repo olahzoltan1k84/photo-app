6d53eb9f44807230d8c28f84dfbed3e9cf3aaded
Oláh Zoltán <olah.zoltan1k84@gmail.com>
2022-03-23 23:00:34 +0200

    Sending Email in Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,9 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'photo-app-olahzoltan1k84.herokuapp.com', :protocol => 'https' }  
+  
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -91,3 +93,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+ 
+
